77f363377b545f0b385b9350b05eae3e86766b69
rbakulin <rbakulin19@gmail.com>
2025-07-14 19:47:27 +0300

    Add 872 comment

--- binary-tree/872-leaf-simular-trees/solution.py
+++ binary-tree/872-leaf-simular-trees/solution.py
@@ -5,14 +5,13 @@ from ..helpers import TreeNode, list_to_tree
 
 def are_leaf_simular(root1: TreeNode, root2: TreeNode) -> bool:
     def _get_all_leafs(root: TreeNode, leaf_list):
-        current = root
-        if current.left is None and current.right is None:
-            leaf_list.append(current.val)
-            return
-        elif current.left:
-            _get_all_leafs(current.left, leaf_list)
-        if current.right:
-            _get_all_leafs(current.right, leaf_list)
+        if root.left is None and root.right is None:
+            leaf_list.append(root.val)
+            return  # go 1 lvl up the call stack, but there's nothing to return - the leaf's already in the list
+        elif root.left:
+            _get_all_leafs(root.left, leaf_list)
+        if root.right:
+            _get_all_leafs(root.right, leaf_list)
     leaf1, leaf2 = [], []
     _get_all_leafs(root1, leaf1)
     _get_all_leafs(root2, leaf2)
